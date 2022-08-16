1d8a99c28c8c84883e787a37a3b0ba9b01c14ffa
Danylo Altukhov <natama1703@gmail.com>
2022-08-16 14:36:02 +0300

    update files

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -3,7 +3,7 @@ const KEY = `19008489-eef4c530baed43ae206c47500`;
 export async function fetchImages(searchItem) {
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${KEY}&q=${searchItem}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=${KEY}&q=${searchItem}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
     );
     return response;
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -1,72 +1,142 @@
 import './css/styles.css';
-import { Notify } from 'notiflix';
-import { fetchImages } from './fetchImages';
+import Notiflix from 'notiflix';
+import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const refs = {
-  searchBar: document.querySelector("[name='searchQuery']"),
-  form: document.querySelector('.search-form'),
+input: document.querySelector('input'),
+form: document.querySelector('.search-form'),
+buttonLoad: document.querySelector('.load-more'),
+gallery: document.querySelector('.gallery'),
+alert: document.querySelector('.alert')
+}
+
+const BASE_URL = "https://pixabay.com/api/";
+
+refs.form.addEventListener('submit', (onFormSubmit));
+refs.buttonLoad.addEventListener('click', (onLoadMoreBtn))
+
+let isAlertVisible = false;
+let nameSearch = refs.input.value;
+let lightbox;
+let currentPage = 1;
+let perPage = 40;
+const totalPages = 500 / perPage;
+console.log(totalPages);
+
+
+refs.buttonLoad.classList.add('invisible');
+
+
+async function fetchImages() {
+    try {
+        const response = await axios.get(`${BASE_URL}?key=19008489-eef4c530baed43ae206c47500&image_type=photo&orientation=horizontal&safesearch=true&q=${nameSearch}&page=${currentPage}&per_page=${perPage}`);
+         const arrayImages = await response.data.hits;
+
+        if(arrayImages.length === 0) {
+            Notiflix.Notify.warning(
+                "Sorry, there are no images matching your search query. Please try again.")
+        }
+        return {arrayImages,
+            totalHits: response.data.totalHits,}       
+        
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+    
+function onFormSubmit(e) {    
+e.preventDefault()
+
+refs.gallery.innerHTML = '';
+nameSearch = refs.input.value;
+nameSearch;
+
+
+  fetchImages() 
+    .then(images => {
+      insertMarkup(images);
+      currentPage += 1;
+    }).catch(error => (console.log(error)))
+
+    refs.buttonLoad.classList.remove('invisible')
+ 
+    lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+  }
+
+
+function onLoadMoreBtn(){
+    if (currentPage > totalPages) {
+        refs.buttonLoad.classList.add('invisible');
+        return toggleAlertPopup()
+    }
+
+    nameSearch = refs.input.value;
+
+    fetchImages() 
+    .then(images => {
+      insertMarkup(images);   
+      currentPage += 1;})
+    .catch(error => (console.log(error)))
+
+    // lightbox.refresh();
+}
+
+
+const createMarkup = img => `
+  <div class="photo-card">
+         <a href="${img.largeImageURL}" class="gallery_link">
+          <img class="gallery__image" src="${img.webformatURL}" alt="${img.tags}"  loading="lazy" />
+          </a>
+        <div class="info">
+              <p class="info-item">
+              <b>Likes<br>${img.likes}</b>
+              </p>
+              <p class="info-item">
+              <b>Views<br>${img.views}</b>
+              </p>
+              <p class="info-item">
+              <b>Comments<br>${img.comments}</b>
+              </p>
+              <p class="info-item">
+              <b>Downloads<br>${img.downloads}</b>
+              </p>
+        </div>
+    </div>
+`;
+      
+
+function generateMarkup(  { arrayImages, totalHits }) {
+    if (currentPage === 1) {
+        Notiflix.Notify.success(`Hoooray! We found ${totalHits} images!`);
+    }
+    return arrayImages.reduce((acc, img) => acc + createMarkup(img), "") 
 };
 
-refs.form.addEventListener('submit', onSearchImages);
 
-function onSearchImages(e) {
-  e.preventDefault();
-
-  const searchItem = refs.searchBar.value;
-
-  fetchImages(searchItem)
-    .then(createMarkup)
-    .then(addMarkup)
-    .catch(errorHandler);
+function insertMarkup(arrayImages) {
+    const result = generateMarkup(arrayImages);
+   
+    refs.gallery.insertAdjacentHTML('beforeend', result);
+ lightbox.refresh();
 }
 
-function createMarkup(imagesArr) {
-  console.log(imagesArr);
-  Notify.info(`Hooray! We found ${imagesArr.data.totalHits} images.`);
-  for (let i = 0; i < imagesArr.length; i += 1) {
-    return `<a class='big__image' href='${i.largeImageURL}'>
-    <div class="photo-card">
-      <img src="${i.pageURL}" alt="${i.tags}" loading="lazy" />
-      <div class="info">
-    <p class="info-item">
-      <b>Likes ${i.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${i.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${i.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${i.downloads}</b>
-    </p>
-  </div>;
-</div>;
-</a>`;
-  }
-}
 
-function errorHandler(error) {
-  if (error === 'not found') {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-}
-<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>;
+function toggleAlertPopup() {
+    if (isAlertVisible) {
+      return;
+    }
+    isAlertVisible = true;
+    refs.alert.classList.add("is-visible");
+    setTimeout(() => {
+      refs.alert.classList.remove("is-visible");
+      isAlertVisible = false;
+    }, 3000);
+  };
